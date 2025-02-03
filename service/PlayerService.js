6109629d2710cdc8f94f6ae51f5f3ce75d113a31
TheCodeRevolution <dataoutputyt@gmail.com>
2025-02-03 17:17:43 +0100

    changed getPlayer function

--- service/PlayerService.js
+++ service/PlayerService.js
@@ -15,13 +15,7 @@ const Player = require('../models/Player.model');
  * language String Language of the Player (e.g., 'de', 'en') (optional)
  * returns BackendPlayer
  **/
-exports.createPlayer = function(uuid, username, playTime, emeralds, language) {
-  console.log("uuid: " + uuid);
-  console.log("username: " + username);
-  console.log("playTime: " + playTime);
-  console.log("emeralds: " + emeralds);
-  console.log("language: " + language);
-
+exports.createPlayer = function (uuid, username, playTime, emeralds, language) {
   var newPlayer = new Player({
     uuid: uuid,
     username: username,
@@ -41,7 +35,7 @@ exports.createPlayer = function(uuid, username, playTime, emeralds, language) {
  * uuid String UUID of the Player
  * returns BackendPlayer
  **/
-exports.deletePlayer = function(uuid) {
+exports.deletePlayer = function (uuid) {
   return new Promise(async (resolve, reject) => {
     try {
       const player = await Player.findOneAndDelete({ uuid: uuid });
@@ -64,7 +58,7 @@ exports.deletePlayer = function(uuid) {
  * uuid String UUID of the Player
  * returns BackendPlayer
  **/
-exports.getPlayer = function(uuid) {
+exports.getPlayer = function (uuid) {
   return new Promise(async (resolve, reject) => {
     try {
       const player = await Player.findOne({ uuid: uuid });
@@ -82,7 +76,7 @@ exports.getPlayer = function(uuid) {
  *
  * returns List
  **/
-exports.getPlayers = function() {
+exports.getPlayers = function () {
   return new Promise(async (resolve, reject) => {
     try {
       const players = await Player.find({});
@@ -107,36 +101,15 @@ exports.getPlayers = function() {
  * language String Language of the Player (e.g., 'de', 'en') (optional)
  * returns BackendPlayer
  **/
-exports.updatePlayer = function(uuid, username, playTime, emeralds, created_at, updated_at, language) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const updateData = {
-        username,
-        playTime,
-        emeralds,
-        language,
-        created_at: new Date(created_at),
-        updated_at: new Date(updated_at)
-      };
+exports.updatePlayer = function (uuid, username, playTime, emeralds, language) {
+  var query = { uuid: uuid };
+  const update = {
+    username,
+    playTime,
+    emeralds,
+    language,
+  };
 
-      const player = await Player.findOneAndUpdate(
-        { uuid: uuid },
-        updateData,
-        { 
-          new: true,          // Returns the updated document
-          runValidators: true // Ensures validators run on update
-        }
-      );
-
-      if (!player) {
-        reject({ code: 404, message: 'Player not found' });
-        return;
-      }
-
-      resolve(player.toAPI());
-    } catch (error) {
-      reject(error);
-    }
-  });
+  return Player.findOneAndUpdate(query, update, { useFindAndModify: false });
 }
 
